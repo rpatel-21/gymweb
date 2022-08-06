e16b4a4dcdc3c1e882e126135de5bcc506563395
Rishabh Patel <educat.warehouse@gmail.com>
2022-08-06 17:16:06 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# READ IT
+html | css | js | projects
+
+This is the project of Gym-site.
+I created it using html and tailwind css.
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'mainbg': "url('../images/home.webp')",
+        'aboutbg': "url('../images/about_bronex.webp')",
+        'classesbg': "url('../images/home.webp')"
+      },
+    },
+  },
+  plugins: [],
+}
